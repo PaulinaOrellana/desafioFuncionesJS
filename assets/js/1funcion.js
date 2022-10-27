c764d5095d325f3d17034dc6e0e1805917c5bafa
PaulinaOrellana <paplina@gmail.com>
2022-10-27 19:06:01 -0300

    Desafio Funciones JS

---
+++ assets/js/1funcion.js
@@ -0,0 +1,9 @@
+function example(a, b, c){
+    return a+b+c
+    }
+
+//función de expresión
+
+example = function(a, b, c){
+    return a+b+c
+    }
---
+++ assets/js/script2.js
@@ -0,0 +1,15 @@
+var colorSeleccionado = '';
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'a') {
+        colorSeleccionado = 'blue';
+    } else if (event.key === 's') {
+        colorSeleccionado = 'green';
+    } else if (event.key === 'd') {
+        colorSeleccionado = 'red';
+    } else if (event.key === 'f') {
+        colorSeleccionado = 'yellow';
+    }
+    console.log(colorSeleccionado)
+    alert(colorSeleccionado)
+});
